e4422444aa5bf477c75c0722a3f50785f93c73f7
Anis Moubarik <moubarik@cs.helsinki.fi>
2012-05-14 14:07:56 +0300

    eka commit

---
+++ sanaindeksi/src/main/java/tira/sanaindeksi/App.java
@@ -0,0 +1,13 @@
+package tira.sanaindeksi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
