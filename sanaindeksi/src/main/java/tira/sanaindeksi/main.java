d7b28bfec23c261a52d542add11477c8d5ecfc45
Anis Moubarik <moubarik@cs.helsinki.fi>
2012-05-14 14:13:25 +0300

    ogkgko

---
+++ sanaindeksi/src/main/java/tira/sanaindeksi/main.java
@@ -0,0 +1,14 @@
+package tira.sanaindeksi;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author moubarik
+ */
+public class main {
+    
+}
