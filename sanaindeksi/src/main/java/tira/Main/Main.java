d0d5ca575a5d7baf5e152f0c011870729a4a4a06
Anis Moubarik <moubarik@cs.helsinki.fi>
2012-05-29 15:27:06 +0300

    Main mukaan

---
+++ sanaindeksi/src/main/java/tira/Main/Main.java
@@ -0,0 +1,16 @@
+package tira.Main;
+
+import java.io.IOException;
+import tira.UI.KomentoRiviUI;
+
+/**
+ *
+ * @author moubarik
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        KomentoRiviUI ui = new KomentoRiviUI();
+        ui.run();
+    }
+
+}
