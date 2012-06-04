b057068df50562022f97c846df5743eadcc747d1
Anis Moubarik <moubarik@cs.helsinki.fi>
2012-06-04 09:55:44 +0300

    testi toimii

--- sanaindeksi/src/test/java/tira/UI/KomentoRiviUITest.java
+++ sanaindeksi/src/test/java/tira/UI/KomentoRiviUITest.java
@@ -6,7 +6,7 @@ package tira.UI;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.Scanner;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /**
@@ -21,19 +21,13 @@ public class KomentoRiviUITest {
      */
     @Test
     public void testLataa() throws Exception {
-        KomentoRiviUI instance = new KomentoRiviUI();
-        instance.run();
-        String komennot = "lataa kalevala.txt\nhae Väinämöinen\nlopeta";
-        InputStream stdin = System.in;
-        try{
-            System.setIn(new ByteArrayInputStream(komennot.getBytes()));
-            Scanner sc = new Scanner(System.in);
-            System.out.println(sc.nextLine());
-        }finally{
-            System.setIn(stdin);
-        }
-
-
+        String komennot = "lataa kalevala.txt\nhae väinämöinen\nlopeta";
+        InputStream in = new ByteArrayInputStream(komennot.getBytes());
+        KomentoRiviUI ui = new KomentoRiviUI(in);
+        
+        ui.run();
+        assertEquals(true, true);
         
     }
+
 }
